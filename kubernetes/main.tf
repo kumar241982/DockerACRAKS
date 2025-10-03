5ab08a0caae7b989e16ce54bec856c7096d810a5
kumar241982 <73668409+kumar241982@users.noreply.github.com>
2025-10-03 20:54:37 +0000

    Update main.tf

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,1 +1,45 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.47.0"
+    }
+  }
+}
 
+provider "azurerm" {
+    features {    
+    }
+
+ 
+}
+
+resource "azurerm_resource_group" "appgrp" {
+  name     = "app-grp"
+  location = "West Europe"
+}
+
+
+
+
+resource "azurerm_kubernetes_cluster" "aks1" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.appgrp.location
+  resource_group_name = azurerm_resource_group.appgrp.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+  depends_on = [ azurerm_resource_group.appgrp ]
+}
