7601878a16e1c99d800e01009691928c40eeb940
kumar241982 <73668409+kumar241982@users.noreply.github.com>
2025-10-04 13:09:52 +0530

    Create main.tf

---
+++ kubernetes+acr/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.47.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {    
+    }
+
+ 
+}
+
+
+resource "azurerm_resource_group" "app67" {
+  name     = "app67-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "acr67" {
+  name                = "containerRegistry67"
+  resource_group_name = azurerm_resource_group.app67.name
+  location            = azurerm_resource_group.app67.location
+  sku                 = "Premium"
+  admin_enabled       = true
+  depends_on = [ azurerm_resource_group.app67 ]
+}
+
+resource "azurerm_kubernetes_cluster" "aks67" {
+  name                = "example-aks67"
+  location            = azurerm_resource_group.app67.location
+  resource_group_name = azurerm_resource_group.app67.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+  depends_on = [ azurerm_resource_group.app67 , azurerm_container_registry.acr67]
+  
+}
+
+resource "azurerm_role_assignment" "kubeide67" {
+  principal_id                     = azurerm_kubernetes_cluster.aks67.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr67.id
+  skip_service_principal_aad_check = true
+
+}
